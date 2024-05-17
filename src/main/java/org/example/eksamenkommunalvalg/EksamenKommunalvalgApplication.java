24771a144fcbae30818c4207dbbe90ee9c5e7756
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-05-17 09:38:42 +0200

    first comit

---
+++ src/main/java/org/example/eksamenkommunalvalg/EksamenKommunalvalgApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamenkommunalvalg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenKommunalvalgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenKommunalvalgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eksamenkommunalvalg/EksamenKommunalvalgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamenkommunalvalg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenKommunalvalgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
